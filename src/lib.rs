34a0887c38207f72e3079e302e6ac240d72cf90d
LAURENT Louis <louis_laurent@aol.fr>
2019-11-24 19:04:29 +0100

    docs(basics): add documentations for basics module functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,62 @@
 pub mod calculatrice_primordial_fonction {
 
     pub mod basics {
+        /// Add two number
+        /// 
+        /// # Exemples
+        /// 
+        /// ```
+        /// let first_number = 10;
+        /// let second_number = 12;
+        /// let result = calculatrice_back::calculatrice_primordial_fonction::basics::add(first_number, second_number);
+        /// 
+        /// assert_eq!(result, 22);
+        /// ```
         pub fn add(first_number: u32, second_number: u32) -> u32 {
             first_number + second_number
         }
     
+        /// Substract two number
+        /// 
+        /// # Exemples
+        /// 
+        /// ```
+        /// let first_number = 10;
+        /// let second_number = 2;
+        /// let result = calculatrice_back::calculatrice_primordial_fonction::basics::sub(first_number, second_number);
+        /// 
+        /// assert_eq!(result, 8);
+        /// ```
         pub fn sub(first_number: u32, second_number: u32) -> u32 {
             first_number - second_number
         }
     
+        /// Multiply two number
+        /// 
+        /// # Exemples
+        /// 
+        /// ```
+        /// let first_number = 10;
+        /// let second_number = 2;
+        /// let result = calculatrice_back::calculatrice_primordial_fonction::basics::mul(first_number, second_number);
+        /// 
+        /// assert_eq!(result, 20);
+        /// ```
         pub fn mul(first_number: u32, second_number: u32) -> u32 {
             first_number * second_number
         }
     
+        /// Divide two number
+        /// 
+        /// # Exemples
+        /// 
+        /// ```
+        /// let first_number = 10;
+        /// let second_number = 2;
+        /// let result = calculatrice_back::calculatrice_primordial_fonction::basics::div(first_number, second_number);
+        /// 
+        /// assert_eq!(result, 5);
+        /// ```
         pub fn div(first_number: u32, second_number: u32) -> u32 {
             first_number / second_number
         }
