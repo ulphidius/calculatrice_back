4f0d72db65a31e78b0225f2d357c79ea266fd552
LAURENT Louis <louis_laurent@aol.fr>
2019-12-04 13:21:33 +0100

    fix(travis): only deploy on tag

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git config --local user.name ${GIT_USER}
-git config --local user.email ${GIT_EMAIL}
+# git config --local user.name ${GIT_USER}
+# git config --local user.email ${GIT_EMAIL}
 
-git tag ${VERSION}
-git push origin ${VERSION}
+# git tag ${VERSION}
+# git push origin ${VERSION}
 
-sed -i "s|version = \"[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*\"|version = \"${VERSION}\"|" ${CARGO_FILE}
+# sed -i "s|version = \"[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*\"|version = \"${VERSION}\"|" ${CARGO_FILE}
